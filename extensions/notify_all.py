6a659e57706fc8025ed5be8c97873262807bf379
Steven Zhou <stevenz9@uci.edu>
2022-10-20 15:50:30 -0700

    finished creating buttons to take in response to water alerts

--- extensions/notify_all.py
+++ extensions/notify_all.py
@@ -1,7 +1,6 @@
 import hikari
 import lightbulb
 import gspread
-import miru
 
 
 lightbulb.BotApp.default_enabled_guilds = []
@@ -14,14 +13,58 @@ async def notify(event: hikari.StartedEvent):
     member_list = await grab_members(member_iterators)
     data_stored = await download()
     available_ids = await upload(member_list, data_stored)
-    # sh.find('id').row gives row number
-    # sh.find('id').col gives col number
-    dm_chat = await notify_plugin.bot.rest.create_dm_channel(available_ids[0])
-    await notify_plugin.bot.rest.add_reaction(dm_chat.id, 1032577215919038555, ":clown:")
+    await msg_all(available_ids)
 
 
+async def update_toggle(toggle: bool, id:str):
+    gc = gspread.service_account(filename='creds.json')
+    sh = gc.open('discordIDs').sheet1
+    sh.update_cell(sh.find(id).row, sh.find(id).col + 1, toggle)
     
 
+async def msg_all(all_ids: list):
+    for id in all_ids:
+        dm_chat = await notify_plugin.bot.rest.create_dm_channel(id)
+        await dm_chat.send("Would you like to be reminded to drink water?", component=await build_btns())
+        alert_toggle = await check_reaction(dm_chat)
+        await update_toggle(alert_toggle, str(id))
+
+
+async def check_reaction(chat_channel):
+    try:
+        button_clicked = await notify_plugin.bot.wait_for(hikari.InteractionCreateEvent, 30)
+        if not isinstance(button_clicked.interaction, hikari.ComponentInteraction):
+            return
+        if button_clicked.interaction.custom_id == 'yes':
+            await button_clicked.interaction.create_initial_response(
+                hikari.ResponseType.MESSAGE_CREATE,
+                "You have decided to turn on water alerts!",
+                flags=hikari.MessageFlag.EPHEMERAL
+            )
+            return True
+        elif button_clicked.interaction.custom_id == 'no':
+            await button_clicked.interaction.create_initial_response(
+                    hikari.ResponseType.MESSAGE_CREATE,
+                    "You have decided to turn off water alerts!",
+                    flags=hikari.MessageFlag.EPHEMERAL
+                )
+            return False
+    except:
+        await chat_channel.send("Your time to answer is up. If you would like to receive water alerts, use command: /toggle")
+        pass
+
+
+async def build_btns():
+    button_row = notify_plugin.bot.rest.build_action_row()
+    yes_button = button_row.add_button(hikari.ButtonStyle.PRIMARY, 'yes')
+    yes_button.set_label('YES')
+    yes_button.add_to_container()
+    no_button = button_row.add_button(hikari.ButtonStyle.DANGER, 'no')
+    no_button.set_label('NO')
+    no_button.add_to_container()
+
+    return button_row
+    
 
 async def upload(member_list, stored_data):
     gc = gspread.service_account(filename='creds.json')
@@ -29,7 +72,7 @@ async def upload(member_list, stored_data):
     available_members = list()
     
     for member in member_list:
-        if (member.user.id not in stored_data) and (member.user.id not in available_members):
+        if (member.user.id not in stored_data) and (member.user.id not in available_members) and (member.user.is_bot == False):
             sh.append_row([str(member.user), str(member.user.id)])
             available_members.append(member.user.id)
         
